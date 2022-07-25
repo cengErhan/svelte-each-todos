a2f6405b84c1cebd756a9d564f57783f1cb44665
Erhan Aslan <erhanjava@gmail.com>
2022-07-25 17:27:06 +0300

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  let todos = ['todo 1', 'todo 2', 'todo 3'];
+  
+  function autoAddTodo(){
+    setTimeout(()=>{
+      todos = [...todos, createRandomString()]
+    }, 1000);
+  }
+
+  autoAddTodo()
+
+  function createRandomString(){
+    let text = (Math.random().toString(36).substring(7))
+    return text
+  }
+
+  function addTodo(){
+    todos = [...todos, createRandomString()]
+  }
+</script>
+
+{#each todos as todo}
+  <p>{todo}</p>
+{/each}
+
+<button on:click={addTodo}>add todo</button>
